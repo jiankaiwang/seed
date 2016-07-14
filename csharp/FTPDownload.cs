885bb8877a6f8f37024811a21a9ce21fde2fed2d
Jian-Kai Wang <s0973078@mail.ncyu.edu.tw>
2016-07-14 23:53:15 +0800

    add FTPDownload.cs and FTPUpload.cs
    1. add FTPDownload class
    2. add FTPUpload class
    3. FTPUpload class needs FTPDownload.cs and MD5HashChecksum.cs

---
+++ csharp/FTPDownload.cs
@@ -0,0 +1,260 @@
+﻿/* author : JinaKai Wang (http://jiankaiwang.no-ip.biz)
+ * github : https://github.com/jiankaiwang/seed
+ * classification : C#
+ * description : download file from the ftp server
+ * --------------------------------------------------
+// method.1 
+FTPDownload fd = new FTPDownload("user","pwd", "ftp://xyz:8020/file.txt");
+int ftpStatus = fd.startFTPDownload();
+if (ftpStatus == 0) {
+    Console.WriteLine(fd.getFTPData());
+}
+
+// method.2
+FTPDownload fd = new FTPDownload("user","pwd", "ftp://xyz:8020/file.txt", "C:/Users/user1/Desktop/example.txt", 10, true);
+int ftpStatus = fd.startFTPDownload();
+ * --------------------------------------------------
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class FTPDownload
+{
+    /*
+     * desc : constructor
+     * parameter :
+     * 1. use ftp user name, ftp password and file path on the server
+     * 2. use ftp user name, ftp password, file path on the server and download file path on the client
+     * 3. use ftp user name, ftp password, file path on the server and count of retring connecting to server 
+     * 4. use ftp user name, ftp password, file path on the server, download file path on the client, count of retring connecting to server and whether download as a file
+     */
+
+    public FTPDownload(String getFTPuser, String getFTPPwd, String getFTPFullDwnPath) {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullDownloadPath = getFTPFullDwnPath;
+        ftpFullLocalPath = String.Empty;
+        downloadAsFile = false;
+        retryCount = 10;
+        ftpData = String.Empty;
+    }
+
+    public FTPDownload(String getFTPuser, String getFTPPwd, String getFTPFullDwnPath, String getFTPFullLocalPath) {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullDownloadPath = getFTPFullDwnPath;
+        ftpFullLocalPath = getFTPFullLocalPath;
+        downloadAsFile = true;
+        retryCount = 10;
+        ftpData = String.Empty;
+    }
+
+    public FTPDownload(String getFTPuser, String getFTPPwd, String getFTPFullDwnPath, int getFTPReTryCount) {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullDownloadPath = getFTPFullDwnPath;
+        ftpFullLocalPath = String.Empty;
+        retryCount = getFTPReTryCount;
+        downloadAsFile = false;
+        ftpData = String.Empty;
+    }
+
+    public FTPDownload(String getFTPuser, String getFTPPwd, String getFTPFullDwnPath, String getFTPFullLocalPath, int getFTPReTryCount, bool dwnFileFlag) {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullDownloadPath = getFTPFullDwnPath;
+        ftpFullLocalPath = getFTPFullLocalPath;
+        retryCount = getFTPReTryCount;
+        downloadAsFile = dwnFileFlag;
+        ftpData = String.Empty;
+    }
+
+    /*
+     * private members
+     */
+
+    private String ftpUser;
+    private String ftpPwd;
+    private String ftpFullDownloadPath;
+    private String ftpFullLocalPath;
+    private int retryCount;
+    private bool downloadAsFile;
+    private String ftpData;
+
+    // ------------------------------
+    // checkFTPDownloadStatus()
+    // desc : check basic parameters are available
+    // ------------------------------
+    private bool checkFTPDownloadStatus() {
+        if (Convert.ToInt32(retryCount) <= 200 && Convert.ToInt32(retryCount) > 0) {
+            return true;
+        }
+        else if (ftpUser.Length > 1 && ftpPwd.Length > 1 && ftpFullDownloadPath.Length > 1 && (!downloadAsFile)) {
+            return true;
+        }
+        else if (ftpUser.Length > 1 && ftpPwd.Length > 1 && ftpFullDownloadPath.Length > 1 && ftpFullLocalPath.Length > 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // ------------------------------
+    // ftpDownloadBody()
+    // desc : the body tries to download data on FTP server as String data type
+    // ------------------------------
+    private String ftpDownloadBody() {
+        String retRes = String.Empty;
+
+        if (!checkFTPDownloadStatus()) {
+            retRes = "-1";
+        }
+        else {
+            // Get the object used to communicate with the ftp server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(String.Format("{0}", ftpFullDownloadPath));
+
+            // set ftp connection type is download
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            // Login FTP Server
+            request.Credentials = new NetworkCredential(ftpUser, ftpPwd);
+
+            // get FTP response
+            try {
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                // convert to stream ready for data stream
+                Stream responseStream = response.GetResponseStream();
+
+                // create a object to read data by stream
+                StreamReader reader = new StreamReader(responseStream);
+
+                // get all data
+                retRes = reader.ReadToEnd();
+
+                // check download status
+                string pattern = "226";
+                foreach (Match match in Regex.Matches(response.StatusDescription, pattern, RegexOptions.IgnoreCase)) {
+                    if (! match.Value.Equals(pattern)) {
+                        retRes = "-3";
+                    }
+                }
+
+                reader.Close();
+                response.Close();
+            }
+            catch {
+                retRes = "-2";
+            }
+        }
+
+        return retRes;
+    }
+
+
+    // ------------------------------
+    // writeIntoFile()
+    // desc : try to write the string into a file
+    // ------------------------------
+    private int writeIntoFile(String getFTPData) {
+        int retStatus = -1;
+
+        for (int reTryIndex = 0; reTryIndex < 2; reTryIndex++) {
+            if (File.Exists(ftpFullLocalPath)) {
+                // file exists
+                using (StreamWriter sw = new StreamWriter(ftpFullLocalPath)) {
+                    sw.Write(getFTPData);
+                    retStatus = 0;
+                }
+            }
+            else {
+                // file did not exist and try to create a new file
+                try {
+                    using (FileStream fs = File.Create(ftpFullLocalPath)) { }
+                } catch { }
+                retStatus = 2;
+            }
+        }
+
+        return retStatus;
+    }
+
+    /*
+     * public members
+     */
+
+    // ------------------------------
+    // startFTPDownload()
+    // desc : main entry to download FTP file
+    // ret :
+    // -1 : ftp basic setting is not prepared
+    // -2 : ftp client can not login the server or file did not exist
+    // -3 : ftp download is not complete
+    // -4 : file exists but is can not be written
+    // -5 : file did not exist or file can not be created
+    // ------------------------------
+    public int startFTPDownload() {
+        int ftpStatus = 0;
+        ftpData = String.Empty;
+
+        for (int ftpUploadCount = 0; ftpUploadCount < retryCount; ftpUploadCount++) {
+            // start to download the ftp data
+            ftpData = ftpDownloadBody();
+
+            if (ftpData.Equals("-1")) {
+                // ftp basis status is not prepared
+                ftpStatus = -1;
+                break;
+            }
+            else if (ftpData.Equals("-2")) {
+                // ftp client can not login the server or file did not exist
+                ftpStatus = -2;
+            }
+            else if (ftpData.Equals("-3")) {
+                // ftp download is not complete
+                ftpStatus = -3;
+            }
+            else {
+                // data is correct
+                ftpStatus = 0;
+                break;
+            }
+        }
+
+        if (ftpStatus == 0 && downloadAsFile) {
+            switch (writeIntoFile(ftpData)) {
+                case 0:
+                    // write file successfully
+                    ftpStatus = 0;
+                    break;
+                case -1:
+                    // file exists but is can not be written
+                    ftpStatus = -4;
+                    break;
+                case 2:
+                    ftpStatus = -5;
+                    // file did not exist
+                    break;
+            }
+        }
+
+        return ftpStatus;
+    }
+
+    // ------------------------------
+    // getFTPData()
+    // desc : get String data type after downloading the file on ftp server
+    // ------------------------------
+    public String getFTPData() {
+        return ftpData;
+    }
+}
---
+++ csharp/FTPUpload.cs
@@ -0,0 +1,266 @@
+﻿/* author : JinaKai Wang (http://jiankaiwang.no-ip.biz)
+ * github : https://github.com/jiankaiwang/seed
+ * classification : C#
+ * description : upload data from to ftp server
+ * necessary outer resource (x2) : 
+ * 1. FTPDownload.cs (https://github.com/jiankaiwang/seed)
+ * 2. MD5HashChecksum.cs (https://github.com/jiankaiwang/seed)
+ * --------------------------------------------------
+// method.1 
+FTPUpload fd = new FTPUpload("user", "pwd", "ftp://xyz:21/example.txt", "Data test", 10, false, true);
+int fdStatus = fd.startFTPUpload();
+Console.WriteLine(String.Format("a{0}", fdStatus));
+Console.Read();
+
+// method.2
+FTPUpload fd = new FTPUpload("user", "pwd", "ftp://xyz:21/example.txt", "C:/Users/user1/Desktop/example1.txt", true);
+int fdStatus = fd.startFTPUpload();
+Console.WriteLine(String.Format("a{0}", fdStatus));
+Console.Read();
+ * --------------------------------------------------
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class FTPUpload
+{
+    /*
+    * constructor
+    * parameter :
+    * 1. use ftp user, user password, full upload path and full local path
+    */
+    public FTPUpload(String getFTPuser, String getFTPPwd, String getFTPFullUpdPath, String getFTPFullLocalPath)
+    {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullUploadPath = getFTPFullUpdPath;
+        ftpFullLocalPath = getFTPFullLocalPath;
+        ftpDataString = String.Empty;
+        retryCount = 10;
+        checkSumFlag = true;
+        uploadFromFile = true;
+    }
+
+    public FTPUpload(String getFTPuser, String getFTPPwd, String getFTPFullUpdPath, String getFTPFullLocalPath, bool getCSFlag) {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullUploadPath = getFTPFullUpdPath;
+        ftpFullLocalPath = getFTPFullLocalPath;
+        ftpDataString = String.Empty;
+        retryCount = 10;
+        checkSumFlag = getCSFlag;
+        uploadFromFile = true;
+    }
+
+    public FTPUpload(String getFTPuser, String getFTPPwd, String getFTPFullUpdPath, String getFTPFullLocalPath, int getReTryCount, bool getCSFlag) {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullUploadPath = getFTPFullUpdPath;
+        ftpFullLocalPath = getFTPFullLocalPath;
+        ftpDataString = String.Empty;
+        retryCount = getReTryCount;
+        checkSumFlag = getCSFlag;
+        uploadFromFile = true;
+    }
+
+    public FTPUpload(String getFTPuser, String getFTPPwd, String getFTPFullUpdPath, String getDataString, int getReTryCount, bool getUploadFromFile, bool getCSFlag)  {
+        ftpUser = getFTPuser;
+        ftpPwd = getFTPPwd;
+        ftpFullUploadPath = getFTPFullUpdPath;
+        ftpFullLocalPath = String.Empty;
+        ftpDataString = getDataString;
+        retryCount = getReTryCount;
+        checkSumFlag = getCSFlag;
+        uploadFromFile = getUploadFromFile;
+    }
+
+    /*
+    * private members
+    */
+
+    private String ftpUser;
+    private String ftpPwd;
+    private String ftpFullUploadPath;
+    private String ftpFullLocalPath;
+    private String ftpDataString;
+    private int retryCount;
+    private bool checkSumFlag;
+    private bool uploadFromFile;
+
+    // ------------------------------
+    // checkUploadSetting()
+    // desc : check ftp upload setting is correct
+    // ------------------------------
+    private bool checkUploadSetting() {
+        if(Convert.ToInt32(retryCount) <= 200 && Convert.ToInt32(retryCount) > 0) {
+            return true;
+        }
+        else if (ftpUser.Length > 1 && ftpPwd.Length > 1 && ftpFullUploadPath.Length > 1 && ftpFullLocalPath.Length > 1 && uploadFromFile) {
+            // upload from file
+            return true;
+        } else if (ftpUser.Length > 1 && ftpPwd.Length > 1 && ftpFullUploadPath.Length > 1 && ftpDataString.Length > 1 && (! uploadFromFile)) {
+            // upload from data string
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // ------------------------------
+    // ftpUploadBody()
+    // desc : ftp upload main body
+    // ------------------------------
+    private int ftpUploadBody(String getDataStr) {
+        int ftpUploadStatus = 0;
+
+        if (! checkUploadSetting()) {
+            ftpUploadStatus = -1;
+        } else {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpFullUploadPath);
+
+            // set ftp connection type to uploadfile
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // Use user and its password to login the ftp server
+            request.Credentials = new NetworkCredential(ftpUser, ftpPwd);
+
+            // Use UTF-8 as encoding type
+            byte[] fileContents = System.Text.Encoding.UTF8.GetBytes(getDataStr);
+
+            // get content length
+            request.ContentLength = fileContents.Length;
+
+            // make sure access to the remote is connected
+            try {
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(fileContents, 0, fileContents.Length);
+                requestStream.Close();
+
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                // regular expression to check 226 status reporting
+                string pattern = "226";
+                foreach (Match match in Regex.Matches(response.StatusDescription, pattern, RegexOptions.IgnoreCase)) {
+                    if (! match.ToString().Equals(pattern)) {
+                        ftpUploadStatus = -3;
+                    }
+                }
+
+                response.Close();
+            }
+            catch {
+                // can not access the ftp server or upload path did not exist
+                ftpUploadStatus = -2;
+            }
+        }
+
+        return ftpUploadStatus;
+    }
+
+    // ------------------------------
+    // getLocalDataContent()
+    // desc : get local file content
+    // ------------------------------
+    private String getLocalDataContent() {
+        String fileContents = String.Empty;
+
+        if (File.Exists(ftpFullLocalPath)) {
+            // Copy the contents of the file to the request stream.
+            StreamReader sourceStream = new StreamReader(ftpFullLocalPath);
+            fileContents = sourceStream.ReadToEnd();
+            sourceStream.Close();
+        }
+        else {
+            fileContents = "-1";
+        }
+
+        return fileContents;
+    }
+
+    /*
+    * public members
+    */
+
+    // ------------------------------
+    // startFTPUpload()
+    // desc : main entry to upload FTP file
+    // ret :
+    // -1 : local file does not exist
+    // -2 : upload setting is not correct
+    // -3 : can not access the ftp server or upload path did not exist
+    // -4 : upload is not complete
+    // -5 : checksum is not the same
+    // ------------------------------    
+    public int startFTPUpload() {
+        int ftpStatus = 0;
+        String getUploadDataString = String.Empty;
+        int ftpUploadBodyFlag = 0;
+
+        for (int rtCnt = 0; rtCnt < retryCount; rtCnt++) {
+            if (uploadFromFile) {
+                // from local file
+                getUploadDataString = getLocalDataContent();
+
+                if (getUploadDataString.Equals("-1")) {
+                    // local file does not exist
+                    ftpStatus = -1;
+                    break;
+                }
+            }
+            else {
+                // from String
+                getUploadDataString = ftpDataString;
+            }
+
+            ftpUploadBodyFlag = ftpUploadBody(getUploadDataString);
+
+            if (ftpUploadBodyFlag == -1) {
+                // upload setting is not correct
+                ftpStatus = -2;
+                break;
+            } else if (ftpUploadBodyFlag == -2) {
+                // can not access the ftp server or upload path did not exist
+                ftpStatus = -3;
+            } else if (ftpUploadBodyFlag == -3) {
+                // upload is not complete
+                ftpStatus = -4;
+            }
+
+            // checksum to make sure data upload is correct
+            if (ftpStatus == 0 && checkSumFlag) {
+                FTPDownload fd = new FTPDownload(ftpUser, ftpPwd, ftpFullUploadPath);
+                int fdStatus = fd.startFTPDownload();
+                if (fdStatus == 0) {
+                    MD5HashChecksum md5obj1;
+                    if (uploadFromFile) {
+                        // from file
+                        md5obj1 = new MD5HashChecksum(fd.getFTPData(), getLocalDataContent());
+                    } else {
+                        // from data string
+                        md5obj1 = new MD5HashChecksum(fd.getFTPData(), ftpDataString);
+                    }   
+                    if (md5obj1.VerifyMd5Hash()) {
+                        ftpStatus = 0;
+                        break;
+                    }
+                    else {
+                        // checksum is not the same
+                        ftpStatus = -5;
+                    }
+                }
+            }
+        }
+
+        return ftpStatus;
+    }
+
+}
