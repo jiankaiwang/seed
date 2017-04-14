2fedef63f73774aa07d30e71851bedd9929c5c10
Jian-Kai Wang <s0973078@mail.ncyu.edu.tw>
2017-04-14 16:22:13 +0800

    add Network.cs
    1. subnet validation

---
+++ csharp/Network.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * proj : seed (https://github.com/jiankaiwang/seed)
+ * auth : JianKai Wang
+ * desc : Network
+ * eg.1 : IsInSameSubnet
+ * var ip1 = IPAddress.Parse("192.168.0.1");
+ * var ip2 = IPAddress.Parse("192.168.1.40");
+ * var mask = IPAddress.Parse("255.255.0.0");
+ * bool inSameNet = ip1.IsInSameSubnet(ip2, mask);
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class IPAddressExtensions
+{
+    public static IPAddress GetBroadcastAddress(this IPAddress address, IPAddress subnetMask)
+    {
+        byte[] ipAdressBytes = address.GetAddressBytes();
+        byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+        if (ipAdressBytes.Length != subnetMaskBytes.Length)
+            throw new ArgumentException("IP or mask Length is error.");
+
+        byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+        for (int i = 0; i < broadcastAddress.Length; i++)
+        {
+            broadcastAddress[i] = (byte)(ipAdressBytes[i] | (subnetMaskBytes[i] ^ 255));
+        }
+        return new IPAddress(broadcastAddress);
+    }
+
+    public static IPAddress GetNetworkAddress(this IPAddress address, IPAddress subnetMask)
+    {
+        byte[] ipAdressBytes = address.GetAddressBytes();
+        byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+        if (ipAdressBytes.Length != subnetMaskBytes.Length)
+            throw new ArgumentException("IP or mask Length is error.");
+
+        byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+        for (int i = 0; i < broadcastAddress.Length; i++)
+        {
+            broadcastAddress[i] = (byte)(ipAdressBytes[i] & (subnetMaskBytes[i]));
+        }
+        return new IPAddress(broadcastAddress);
+    }
+
+    public static bool IsInSameSubnet(this IPAddress address2, IPAddress address, IPAddress subnetMask)
+    {
+        IPAddress network1 = address.GetNetworkAddress(subnetMask);
+        IPAddress network2 = address2.GetNetworkAddress(subnetMask);
+
+        return network1.Equals(network2);
+    }
+}
