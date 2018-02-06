4d3cc7b52a4eb09dceda6bb3ebc662f7276ffbf7
Jian-Kai Wang <s0973078@mail.ncyu.edu.tw>
2018-02-06 15:41:16 +0800

    add handlers to transform from unicode to utf8
    deal with \\u.... condition

--- python/TEXTCODING.py
+++ python/TEXTCODING.py
@@ -1,7 +1,8 @@
 #
-# author : jiankaiwang (https://welcome-jiankaiwang.rhcloud.com/)
+# author : jiankaiwang (https://jiankaiwang.no-ip.biz/)
 # project : seed (https://github.com/jiankaiwang/seed)
 # reference : seed (https://www.gitbook.com/book/jiankaiwang/seed/details)
+# resource: TEXTCODING
 #
 
 # coding: utf-8
@@ -9,7 +10,7 @@
 #
 # desc : unicode to utf8 from list
 # retn : a utf8-encoding list
-# e.g. : unicode2utf8FromList([ u'\u4e3b\u8981' : u'\u4e3b\u8981' ]) 
+# e.g. : unicode2utf8FromList([ u'\u4e3b\u8981' : u'\u4e3b\u8981' ])
 #
 def unicode2utf8FromList(getList):
     newList = []
@@ -19,29 +20,34 @@ def unicode2utf8FromList(getList):
         elif isinstance(item,dict):
             newList.append(unicode2utf8FromDict(item))
         else:
-            newList.append(unicode(item).encode('utf-8'))
+            newList.append(unicode2utf8FromStr(item))
     return newList
 
 #
 # desc : unicode to utf8 from dictionary
 # retn : a utf8-encoding dictionary
-# e.g. : unicode2utf8FromDict({ u'\u4e3b\u8981' : u'\u4e3b\u8981' }) 
+# e.g. : unicode2utf8FromDict({ u'\u4e3b\u8981' : u'\u4e3b\u8981' })
 #
 def unicode2utf8FromDict(getDict):
     newData = {}
     for k, v in getDict.iteritems():
         if isinstance(v,list):
-            newData[k] = unicode2utf8FromList(v)
+            newData[unicode2utf8FromStr(k)] = unicode2utf8FromList(v)
         elif isinstance(v,dict):
-            newData[k] = unicode2utf8FromDict(v)
+            newData[unicode2utf8FromStr(k)] = unicode2utf8FromDict(v)
         else:
-            newData[k] = unicode(v).encode('utf-8')
+            newData[k] = unicode2utf8FromStr(v)
     return newData
 
 #
 # desc : unicode to utf8 from string
 # retn : a utf8-encoding string
 # e.g. : unicode2utf8FromStr(u'\u4e3b\u8981')
-#       
+#
 def unicode2utf8FromStr(getStr):
-    return unicode(getStr).encode('utf-8')
+    try:
+        # deal with \\u....
+        return unicode(getStr,'unicode-escape').encode('utf-8')
+    except TypeError:
+        return unicode(getStr).encode('utf-8')
+        
